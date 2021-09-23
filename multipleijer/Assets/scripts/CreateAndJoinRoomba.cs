0b1ee7bfe6d4e8f4e0dc07b438e431f9fd5b97c5
Aaro <aaro.lintulahti@gmail.com>
2021-09-23 19:33:51 +0300

    grass

--- multipleijer/Assets/scripts/CreateAndJoinRoomba.cs
+++ multipleijer/Assets/scripts/CreateAndJoinRoomba.cs
@@ -8,15 +8,33 @@ using Photon.Pun;
 public class CreateAndJoinRoomba : MonoBehaviourPunCallbacks
 {
     public TMP_InputField createInput;
+
+    public TMP_InputField joinInput;
     // Start is called before the first frame update
-    void Start()
+    public void CreateRoom()
     {
+        //new room go brrrrr
+        PhotonNetwork.CreateRoom(createInput.text);
+
         
     }
 
-    // Update is called once per frame
-    void Update()
+
+    public void JoinRoom()
     {
-        
+        PhotonNetwork.JoinRoom(joinInput.text);
+
+
     }
+
+
+
+    public override void OnJoinedRoom()
+    {
+        PhotonNetwork.LoadLevel("peli");
+
+
+    }
+
+
 }
