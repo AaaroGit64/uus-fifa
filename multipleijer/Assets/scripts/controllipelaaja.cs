3ccbdb08aed847377b8a1f55ed84cf02032eb55a
Aaro <aaro.lintulahti@gmail.com>
2021-10-07 19:35:32 +0300

    gravity

--- multipleijer/Assets/scripts/controllipelaaja.cs
+++ multipleijer/Assets/scripts/controllipelaaja.cs
@@ -2,34 +2,53 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-CharacterController Controller : MonoBehaviour
-
-public float moveSpeed = 16F;
-
-public float runSpeed = 99F;
-
-public float jumpHeight 3F;
-
-public float gravity = -9F;
-
-public Transform groundCheck;
-
-public float groundDistance = 0.1f;
-public LayerMask groundMask;
-
-[SerializeField] private bool isGrounded;
-
 public class controllipelaaja : MonoBehaviour
+
 {
-    // Start is called before the first frame update
+    CharacterController Controller;
+    public float moveSpeed = 16f;
+
+    public float runSpeed = 99f;
+
+    public float jumpHeight = 3f;
+
+    public float gravity = -9f;
+
+    public Transform groundCheck;
+
+    public float groundDistance = 0.1f;
+    public LayerMask groundMask;
+
+    [SerializeField] private bool isGrounded;
+    
+    private Vector3 velocity;
+    
+        // Start is called before the first frame update
     void Start()
     {
-        
+        Controller = GetComponent<CharacterController>();
     }
 
-    // Update is called once per frame
-    void Update()
+     // Update is called once per frame
+     void Update()
     {
+        CheckIfGrounded();  
+    }
+    
+    private void CheckIfGrounded()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position,groundDistance,groundMask);
+
+        if(isGrounded)
+        {
+           velocity.y = -2;
+
+
+        }
+
+        velocity.y +=gravity * Time.deltaTime;
+        Controller.Move(velocity * Time.deltaTime);
         
     }
 }
+
